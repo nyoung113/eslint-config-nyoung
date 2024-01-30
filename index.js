7c38b20f3fe6279bbd7fdbdc72687a8b2c4551d6
nyoung113 <mysoulk0113@gmail.com>
2024-01-30 20:26:53 +0900

    feat: eslint 설정

---
+++ index.js
@@ -0,0 +1,40 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    "extends": ["eslint:recommended"],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true,
+        },
+    },
+    env: {
+        browser: true,
+        es6: true,
+        node: true,
+    },
+    plugins: [
+        '@typescript-eslint',
+        '@tanstack/query',
+        'import',
+        'jsdoc',
+        'jsx-a11y',
+        'prettier',
+        'react',
+        'react-hooks',
+        'unused-imports',
+    ],
+    // 사용할 규칙 설정
+    "rules": {
+        'import/extensions': [
+            'error',
+            {
+                js: 'never',
+                jsx: 'never',
+                ts: 'never',
+                tsx: 'never',
+                json: 'ignorePackages',
+            },
+        ],
+    },
+}
