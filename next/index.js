48624efc42a0969f9982c76fa6e3296adf541e5f
nyoung113 <mysoulk0113@gmail.com>
2024-02-10 12:51:08 +0900

    config: default 설정 파일 공유

--- next/index.js
+++ next/index.js
@@ -1,41 +1,7 @@
 module.exports = {
-    parser: '@typescript-eslint/parser',
-    "extends": ["eslint:recommended"],
-    parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-        ecmaFeatures: {
-            jsx: true,
-        },
-    },
-    env: {
-        browser: true,
-        es6: true,
-        node: true,
-    },
+    "extends" : ['/lib/default'],
     plugins: [
-        '@typescript-eslint',
-        '@tanstack/query',
-        'import',
-        'jsdoc',
-        'jsx-a11y',
-        'prettier',
-        'react',
-        'react-hooks',
-        'unused-imports',
         'next'
     ],
-    // 사용할 규칙 설정
-    "rules": {
-        'import/extensions': [
-            'error',
-            {
-                js: 'never',
-                jsx: 'never',
-                ts: 'never',
-                tsx: 'never',
-                json: 'ignorePackages',
-            },
-        ],
-    },
+
 }
